f044a92b12fa691b200513eaf69eadbc97d9168a
raikwarshubham1997 <raikwarshubham.sr@gmail.com>
2024-02-20 11:38:01 +0530

    Update

---
+++ CaretakerSystem/CaretakerSystem/app/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+admin.site.register(Role)
+admin.site.register(User)
+admin.site.register(PayrollGroup)
+admin.site.register(Country)
+admin.site.register(Language)
+admin.site.register(Employment)
+admin.site.register(Ethnicity)
+admin.site.register(EqualityAndDiversity)
+admin.site.register(Regions)
+admin.site.register(BankDetails)
+admin.site.register(RegionsOption)
+
